c501610a5e8ed4e14e41b68ab1fa9364a99a66da
Sam <sam@developersam.com>
2020-12-26 23:18:47 -0500

    [rust-rewrite] Implement simple stdio based CLI

--- rust-rewrite/src/main.rs
+++ rust-rewrite/src/main.rs
@@ -9,13 +9,17 @@ mod renamer;
 mod runtime;
 mod transformer;
 
+use std::io::{self, Read};
+
 fn main() {
-  let program =
-    "var a = 4; fun b(v: int): int = v fun main(): void = if true then serve(b(37) + 1 + a) else ({})"
-      .to_string();
-  match checker::get_type_checked_program(runtime::get_critter_world_runtime(), program) {
-    Ok(p) => {
-      println!("{:}", compiler::compile_to_critter_lang(p, 20));
+  let mut program_buffer = String::new();
+  match io::stdin().read_to_string(&mut program_buffer) {
+    Ok(_) => (),
+    Err(e) => panic!(e),
+  }
+  match checker::get_type_checked_program(runtime::get_critter_world_runtime(), program_buffer) {
+    Ok(program) => {
+      println!("{:}", compiler::compile_to_critter_lang(program, 20));
     }
     Err(e) => {
       println!("Errors: {:?}", e);
